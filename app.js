df2acb137461f973553ab8aeb34d37185ac4289e
Cesar Ribeiro <cesartertulianort@gmail.com>
2024-09-18 17:36:49 -0300

    test

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log('Ola cesar, em breve serei uma API!!!');
+import express from 'express'
 
+const servidor = express();
+
+servidor.get('helloword', (req, resp) => {
+    //codigo endpoint
+    resp.send('hello word!!!')
+})
+
+
+servidor.listen(5001,
+    () => console.log('API subiu com sucesso'))
